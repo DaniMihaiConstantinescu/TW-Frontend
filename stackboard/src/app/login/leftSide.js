188f131fa2e931991a7648bb37600a25cdb66edf
DaniConstantinescu <mihai.constantinescu02@e-uvt.ro>
2023-10-25 22:04:21 +0300

    Modules + InputText
    Created the input fields and added the needed modules

--- stackboard/src/app/login/leftSide.js
+++ stackboard/src/app/login/leftSide.js
@@ -1,8 +1,61 @@
+"use client";
+
+import styles from '@/css/auth.module.css'
+import { styled } from '@mui/material/styles';
+import { IconButton, InputAdornment, TextField } from "@mui/material";
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import { useState } from 'react';
+
+const InputField = styled(TextField)({
+    '& label.Mui-focused': {
+      color: '#A0AAB4',
+    },
+    '& .MuiInput-underline:after': {
+      borderBottomColor: '#B2BAC2',
+    },
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderColor: '#6f7d90',
+      },
+      '&:hover fieldset': {
+        borderColor: '#B2BAC2',
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: '#6F7E8C',
+      },
+    },
+  });
+
 export default function LeftSide() {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
-    <div>
-        <h1>StackBoard</h1>
-        <h3>Sign in to the account</h3>
+    <div className={styles.leftContainer}>
+      <h1>StackBoard</h1>
+      <h3>Sign in to the account</h3>
+
+      <form className={styles.form}>
+        <InputField type='email' label="Email"/>
+
+        <InputField
+          label="Password"
+          type={showPassword ? 'text' : 'password'}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  onClick={() => setShowPassword(!showPassword)}
+                  edge="end"
+                >
+                  {showPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+
+      </form>
     </div>
-  )
+  );
 }
